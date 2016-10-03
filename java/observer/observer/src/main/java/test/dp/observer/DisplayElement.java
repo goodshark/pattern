4eb9d3984f4cd7f1ca1b1b27c76fa8055636184a
邓人菠 <dengrenbo@goodday.local>
2016-10-03 10:15:06 +0800

    add observer dp

---
+++ java/observer/observer/src/main/java/test/dp/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package test.dp.observer;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ java/observer/observer/src/main/java/test/dp/observer/Observer.java
@@ -0,0 +1,5 @@
+package test.dp.observer;
+
+public interface Observer {
+	public void update(float temp, float humidity, float pressure);
+}
